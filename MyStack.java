b30e420fb78fbde9aeb0470925d7be479860ad6a
naveeny061 <naveeny061@gmail.com>
2020-11-30 23:45:22 +0530

    Added:Push Method and Print Method

--- MyStack.java
+++ MyStack.java
@@ -1,7 +1,35 @@
-package com.stack;
+ package com.stack;
+
+import com.assignment.INode;
+import com.assignment.MyLinkList;
+import com.assignment.MyNode;
+
 
 public class MyStack {
+	public final MyLinkList myLinkList;
+
+	public MyStack() {
+		
+		this.myLinkList = new MyLinkList();
+	}
+
+	public void push(INode myNode) {
+		myLinkList.add(myNode);
+	}
+
+	public void printStack() {
+		myLinkList.printMyNodes();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack and Queue Problem");
+		MyStack myStack = new MyStack();
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		myStack.printStack();
 	}
 }
